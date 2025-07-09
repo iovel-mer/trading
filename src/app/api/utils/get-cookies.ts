9c93746a612f7277c12260e35456c2102ecdf44a
bekacholokava <bcholokava@gmail.com>
2025-07-09 14:10:37 +0400

    project setup

---
+++ src/app/api/utils/get-cookies.ts
@@ -0,0 +1,10 @@
+import { cookies } from "next/headers";
+
+export const getAccessToken = async () => {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+
+  if (!accessToken) return undefined;
+
+  return accessToken;
+};
